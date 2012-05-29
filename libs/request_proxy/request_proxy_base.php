4c9d67ead8ca91c00cbf28fd59e10b8fed355bf0
Graham Weldon <graham@grahamweldon.com>
2012-05-30 09:57:42 +1000

    Simplified code. Change " to ' and fix coding standards.

--- libs/request_proxy/request_proxy_base.php
+++ libs/request_proxy/request_proxy_base.php
@@ -12,12 +12,10 @@
 if (!function_exists('apache_request_headers')) { 
 	function apache_request_headers() { 
 		foreach($_SERVER as $key => $value) { 
-			if (substr($key, 0, 5) == "HTTP_") { 
-				$key = str_replace(" ","-",ucwords(strtolower(str_replace("_", " ", substr($key, 5))))); 
-				$out[$key] = $value; 
-			} else { 
-				$out[$key] = $value; 
+			if (substr($key, 0, 5) === 'HTTP_') { 
+				$key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($key, 5))))); 
 			} 
+			$out[$key] = $value; 
 		} 
 		return $out; 
 	} 
