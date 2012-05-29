2a7a270692732876ee4dd243bfa0e631357ecd7a
Aristides Castillo <aristidescc@gmail.com>
2012-05-29 11:18:55 -0430

    Enforced code conventions to last fix

--- libs/request_proxy/request_proxy_base.php
+++ libs/request_proxy/request_proxy_base.php
@@ -11,12 +11,12 @@
 
 if (!function_exists('apache_request_headers')) { 
 	function apache_request_headers() { 
-		foreach($_SERVER as $key=>$value) { 
-			if (substr($key,0,5)=="HTTP_") { 
-				$key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5))))); 
-				$out[$key]=$value; 
+		foreach($_SERVER as $key => $value) { 
+			if (substr($key, 0, 5) == "HTTP_") { 
+				$key = str_replace(" ","-",ucwords(strtolower(str_replace("_", " ", substr($key, 5))))); 
+				$out[$key] = $value; 
 			} else { 
-				$out[$key]=$value; 
+				$out[$key] = $value; 
 			} 
 		} 
 		return $out; 
